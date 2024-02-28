4ab504857e6d9e22c24111dd11e2f00d71a3dfb6
Jindrich Susen <jindrich.susen@advantages.cz>
2024-02-28 19:49:26 +0100

    + Clicking on links in mobile menu closes the menu

--- _includes/scripts/components/nav.js
+++ _includes/scripts/components/nav.js
@@ -1,7 +1,8 @@
 const nav = () => {
-	var $nav = document.querySelector(".l-header");
-	var $html = document.querySelector("html");
-	const $toggleBtn = document.querySelector(".menu-toggle");
+	var nav = document.querySelector(".l-header");
+	var html = document.querySelector("html");
+	const toggleButton = document.querySelector(".menu-toggle");
+	const headerLinks = document.getElementsByClassName("header-link");
 
 	function stickyNav() {
 		window.addEventListener("scroll", function () {
@@ -14,18 +15,24 @@ const nav = () => {
 				return;
 
 			if (this.window.scrollY > 0) {
-				$nav.classList.add("scrolling");
+				nav.classList.add("scrolling");
 			} else {
-				$nav.classList.remove("scrolling");
+				nav.classList.remove("scrolling");
 			}
 		});
 	}
 
 	function init() {
-		$toggleBtn.addEventListener("click", function () {
-			$html.classList.toggle("overflow-hidden");
-			$nav.classList.toggle("menu-visible");
+		toggleButton.addEventListener("click", function () {
+			html.classList.toggle("overflow-hidden");
+			nav.classList.toggle("menu-visible");
 		});
+		for (const headerLink of headerLinks) {
+			headerLink.addEventListener("click", function () {
+				html.classList.remove("overflow-hidden");
+				nav.classList.remove("menu-visible");
+			});
+		}
 
 		stickyNav();
 	}
